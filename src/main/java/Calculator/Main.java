2fa4ee15a61a67743f59f9549c2589d68c99ccdd
user325 <user325@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-04-03 12:44:48 +0000

    Добавление расчётной формулы аннуитетного платежа.

--- src/main/java/Calculator/Main.java
+++ src/main/java/Calculator/Main.java
@@ -1,5 +1,6 @@
 package Calculator;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -20,7 +21,7 @@ public class Main {
 		Main_Panel.add(FirstLabel);
 		FirstLabel.setBounds(20, 80, 260, 20);
 		FirstLabel.setVisible(true);
-		JTextField FirstField = new JTextField();
+		final JTextField FirstField = new JTextField();
 		Main_Panel.add(FirstField);
 		FirstField.setBounds(20, 100, 260, 20);
 		FirstField.setVisible(true);
@@ -28,15 +29,15 @@ public class Main {
 		Main_Panel.add(SecondLabel);
 		SecondLabel.setBounds(20, 120, 260, 20);
 		SecondLabel.setVisible(true);
-		JTextField SecondField = new JTextField();
+		final JTextField SecondField = new JTextField();
 		Main_Panel.add(SecondField);
 		SecondField.setBounds(20, 140, 260, 20);
 		SecondField.setVisible(true);
-		JLabel ThirdLabel = new JLabel("Месячная ставка по кредиту");
+		JLabel ThirdLabel = new JLabel("Годовая ставка по кредиту");
 		Main_Panel.add(ThirdLabel);
 		ThirdLabel.setBounds(20, 160, 260, 20);
 		ThirdLabel.setVisible(true);
-		JTextField ThirdField = new JTextField();
+		final JTextField ThirdField = new JTextField();
 		Main_Panel.add(ThirdField);
 		ThirdField.setBounds(20, 180, 260, 20);
 		ThirdField.setVisible(true);
@@ -44,7 +45,7 @@ public class Main {
 		Main_Panel.add(FourthLabel);
 		FourthLabel.setBounds(320, 100, 260, 20);
 		FourthLabel.setVisible(true);
-		JLabel FifthLabel = new JLabel("");
+		final JLabel FifthLabel = new JLabel("");
 		Main_Panel.add(FifthLabel);
 		FifthLabel.setBounds(320, 120, 260, 20);
 		FifthLabel.setVisible(false);
@@ -52,7 +53,7 @@ public class Main {
 		Main_Panel.add(SixthLabel);
 		SixthLabel.setBounds(320, 140, 260, 20);
 		SixthLabel.setVisible(true);
-		JLabel SeventhLabel = new JLabel("");
+		final JLabel SeventhLabel = new JLabel("");
 		Main_Panel.add(SeventhLabel);
 		SeventhLabel.setBounds(320, 160, 260, 20);
 		SeventhLabel.setVisible(false);
@@ -64,10 +65,37 @@ public class Main {
 		ActionListener InforListener = new Information();
 		Information.addActionListener(InforListener);
 		
+		class Raschet extends Main implements ActionListener {
+
+			private double A;
+
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String S_stroka = FirstField.getText();
+				Integer S = Integer.parseInt(S_stroka);
+				String M_stroka = SecondField.getText();
+				Integer M = Integer.parseInt(M_stroka);
+				String P_stroka = ThirdField.getText();
+				float P = Integer.parseInt(P_stroka);
+				 P = (float) P / 1200;
+				 A = S * P / ( 1 - Math.pow(1 + P, -M));
+				 A = Math.ceil(A);
+				 double H = A * M;
+				SeventhLabel.setText("" + H);
+				FifthLabel.setText("" + A);
+				FifthLabel.setVisible(true);
+				SeventhLabel.setVisible(true);
+				
+			}
+			
+		}
+		
 		JButton Calculated = new JButton("Расчитать");
 		Calculated.setBounds(160, 300, 120, 30);
 		Main_Panel.add(Calculated);
 		Calculated.setVisible(true);
+		ActionListener Calculat = new Raschet();
+		Calculated.addActionListener(Calculat);
 		
 		JButton Exit = new JButton("Выход");
 		Exit.setBounds(460, 300, 100, 30);
